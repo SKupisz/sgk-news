f112338dda38f37919f37f0dd51349c39b7c1c49
SKupisz <szym-ku@wp.pl>
2019-11-28 21:29:10 +0100

    Single bulletin piece logic, design and RWD done

--- bulletin.php
+++ bulletin.php
@@ -9,7 +9,7 @@ require_once "./src/bulletin/loadData.php";
       <meta name="viewport"  content="width=device-width, inital-scale=1.0"/>
       <link rel="stylesheet" type="text/css" href = "main/bar.css"/>
       <link rel = "stylesheet" type = "text/css" href="src/bulletin/css/main.css"/>
-      <link rel="shortcut icon" type = "image/png" href = "main/logo.png"/>
+      <link rel="shortcut icon" type = "image/png" href = "./main/logo.png"/>
       <meta name="description" content="SGK-news website">
       <meta name="keywords" content="SGK-news, news, daily, buisness, politic,art,Simon Kupisz">
     <title>SGK news</title>
@@ -26,20 +26,33 @@ require_once "./src/bulletin/loadData.php";
                 </section>
             <?php
         }
-        else{
+        else if($isConnection == 1){
             ?><header class="welcome-header">Service bulletin</header><?php
             for($i = 0 ;$i < $howMany; $i++){
                 $row = $rows[$i];
                 ?>
+                <a href = "?u=<?php echo $row['id'];?>">
                     <section class="content-container">
+                        
                         <div class="bull-container">
                             <header class="bull-header"><?php echo $row["title"];?></header>
                             <div class="bull-desc"><?php echo (strlen($row["content"]) < 200) ? $row["content"] : substr($row["content"],0,197)."...";?></div>
                             <footer class="bull-date"><?php echo $row["publishingDate"];?></footer>
                         </div>
+            
                     </section>
+                    </a>
                 <?php
             }
+        }
+        else{
+            ?>
+            <header class="bulletin-header"><?php echo $title;?></header>
+            <section class="bulletin-container">
+                <div class="bulletin-content"><?php echo $content; ?></div>
+                <footer class="bulletin-date"><?php echo $date;?></footer>
+            </section>
+            <?php
         }
         ?>
     </main>
--- src/bulletin/loadData.php
+++ src/bulletin/loadData.php
@@ -6,14 +6,40 @@ try{
     $connection = new PDO('mysql:host='.$host.';dbname='.$db_name,$db_user,$db_password,
     [PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    $getTheBulletinContent = $connection->prepare("SELECT * FROM bulletins ORDER BY publishingDate DESC");
-    $getTheBulletinContent->execute();
-    $howMany = $getTheBulletinContent->rowCount();
-    $rows = array();
-    for($i = 0 ; $i < $howMany; $i++){
-        $row = $getTheBulletinContent->fetch(PDO::FETCH_ASSOC);
-        $rows[$i] = $row;
+    if(!isset($_GET["u"])){
+        $getTheBulletinContent = $connection->prepare("SELECT * FROM bulletins ORDER BY publishingDate DESC");
+        $getTheBulletinContent->execute();
+        $howMany = $getTheBulletinContent->rowCount();
+        $rows = array();
+        for($i = 0 ; $i < $howMany; $i++){
+            $row = $getTheBulletinContent->fetch(PDO::FETCH_ASSOC);
+            $rows[$i] = $row;
+        }
     }
+    else{
+        $number_first = $_GET["u"];
+        $number = (int)$number_first;
+        $numberTest = (string)$number;
+        if($number != $number_first){
+            $isConnection = 0;
+        }
+        else{
+            $getThisBulletin = $connection->prepare("SELECT * FROM bulletins WHERE id = :id");
+            $getThisBulletin->bindValue(":id",$number,PDO::PARAM_INT);
+            $getThisBulletin->execute();
+            $howMany = $getThisBulletin->rowCount();
+            if($howMany == 0) $isConnection = 0;
+            else{
+                $row = $getThisBulletin->fetch(PDO::FETCH_ASSOC);
+                $title = $row["title"];
+                $content = $row["content"];
+                $date = $row["publishingDate"];
+                $isConnection = 2;
+            }
+        }
+
+    }
+
     $connection = null;
 } catch(Exception $e){
     $isConnection = 0;
